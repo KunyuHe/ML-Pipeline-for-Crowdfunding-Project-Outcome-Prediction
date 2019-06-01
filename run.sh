77df4e6e1005b1b8307d34ddc2d902f6ab8fe023
Kunyu He <kunyuhe@uchicago.edu>
2019-06-01 03:33:37 -0500

    Modified shell script to create virtual env, intall and run silently

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+py -m venv env
+.\env\Scripts\activate
+
 pip install --user -r requirements.txt
+
 
 cd ./codes
 python etl.py
-start 
-python featureEngineering.py
-python train.py
+start https://mybinder.org/v2/gh/KunyuHe/ML-Pipeline-for-Crowdfunding-Project-Outcome-Prediction/master?filepath=%2FEDA%2FEDA.ipynb
+python featureEngineering.py --ask_user 0 --verbose 1
+python train.py --clean_start 0 --ask_user 0 --verbose 1 --plot 1
+
+deactivate
